d14c7655c1a04f599db609bfe9c02617cb21b963
ne_sanya <barinovaaalexandra@gmail.com>
2020-03-09 22:36:07 +0300

    FragmentSettings updated (xml file)

--- app/src/main/java/com/example/projectgame/SettingsFragment.java
+++ app/src/main/java/com/example/projectgame/SettingsFragment.java
@@ -14,18 +14,14 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment {
-
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
+    public View view;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        view = inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
     }
 
 }
