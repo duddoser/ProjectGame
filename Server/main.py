539121c48a10362a7e3f312670e39b13c62bcfc1
duddoser <45271863+duddoser@users.noreply.github.com>
2020-11-22 17:09:44 +0300

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -16,38 +16,43 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-
 @app.route('/login', methods=["POST"])
-def login(username, password, email):
-    user_model.insert_newuser(username, password, email, 0, 0, 0, 0, "", "", 0, 0)
-    exists = user_model.exists(username, password)
+def login():
+    result = request.get_json()
+    exists = user_model.exists(result['username'])
     if not exists[0]:
-        session['username'] = username
-        session['user_id'] = exists[1]
+        user_model.insert_newuser(result['username'], result['password'], result['email'],
+                              100, 100, 100, 100, "", "", 0, 0)
+        return jsonify({"message": "success"})
+    return jsonify({"message": "none"})
 
 
-@app.route('/auth', methods=["GET"])
-def auth(username, password):
-    exists = user_model.exists(username, password)
+@app.route('/auth', methods=["POST"])
+def auth():
+    result = request.get_json()
+    exists = user_model.exists(result['username'], result['password'])
     if exists[0]:
-        session['username'] = username
-        session['user_id'] = exists[1]
+        return jsonify({"message": "success"})
+    return jsonify({"message": "none"})
 
 
-@app.route('/resources', methods=["GET"])
+
+@app.route('/resources', methods=["POST"])
 def get_all_resources():
-    # res = list(user_model.get_all(session['user_id']))
+    result = request.get_json()
+    res = list(user_model.get_all(result['user_id']))
     snippet = {'wood': 0, 'iron': 0, 'metal': 0, 'stone': 0}
-    # i = 0
-    # for el in snippet:
-    #     snippet[el] = res[i]
-    #     i += 1
+    i = 0
+    for el in snippet:
+        snippet[el] = res[i]
+        i += 1
     return jsonify(snippet)
 
 
 @app.route('/change_res', methods=["POST"])
-def change_resource(res, num):
-    user_model.update_resource(res, num, session['user_id'])
+def change_resource():
+    result = request.get_json()
+    user_model.update_resource(result['resource'], result['amount'], result['user_id'])
 
 
 @app.route('/get_res', methods=["GET"])
@@ -56,41 +61,63 @@ def get_resource(res):
     return make_response(jsonify({reses[0]: reses[1]}))
 
 
+@app.route('/get_buildings', methods=['POST'])
+def get_buildings():
+    result = request.get_json()
+    buildings = list(user_model.get_buildings(result['user_id']))
+    snippet = {'tawnhall': 0, 'barraks': 0, 'shack': 0, 'farm': 0, 'house': 0, 'warehouse': 0,
+               'big_house': 0, 'church': 0}
+    i = 0
+    for b in snippet:
+        snippet[b] = buildings[i]
+        i += 1
+    return jsonify(snippet)
+
+
+@app.route('/set_buildings', methods=['POST'])
+def set_buildings():
+    result = request.get_json()
+    user_model.update_building(result['building'], result['user_id'])
+    return jsonify({"message": "success"})
+
+
 @app.route('/set_trade', methods=["POST"])
-def set_trade(obtain_res, give_res, obtain_num, give_num):
-    user_model.set_trade(session['user_id'], obtain_res, give_res, obtain_num, give_num)
+def set_trade():
+    result = request.get_json()
+    user_model.set_trade(result['user_id'], result['obtain_res'], result['give_res'], result['obtainInt'], result['giveInt'])
+    return jsonify({"message": "success"})
 
 
-@app.route('/get_trades', methods=["GET"])
+@app.route('/get_trades', methods=["POST"])
 def get_trades():
     trades = list(user_model.get_trades())
-    snippet = {}
-    i = 0
-    trades_u = []
-    k, j = 0, 5
-    for _ in range(len(trades)//5):
-        trades_u.append(trades[k:j])
-        k += 5
-        j += 5
+    # trades_u = []
+    # k, j = 0, 5
+    # for _ in range(len(trades)//5):
+    #     trades_u.append(trades[k:j])
+    #     k += 5
+    #     j += 5
 
-    prep = {}
-    for row in range(len(trades_u)):
-        prep['obtain_res'] = trades_u[row][1]
-        prep['give_res'] = trades_u[row][2]
-        prep['obtain_num'] = trades_u[row][3]
-        prep['give_num'] = trades_u[row][4]
-        snippet[trades_u[0]] = prep
-        prep = {}
+    # data = []
+    # prep = {}
+    # for row in range(len(trades_u)):
+    #     prep['user_id'] = trades_u[row][0]
+    #     prep['obtain_res'] = trades_u[row][1]
+    #     prep['give_res'] = trades_u[row][2]
+    #     prep['obtainInt'] = trades_u[row][3]
+    #     prep['giveInt'] = trades_u[row][4]
+    #     data.append(prep)
+    #     prep = {}
 
-    return make_response(jsonify(snippet))
+    return jsonify(trades)
 
 
-@app.route('/logout')
-def logout():
-    session.pop('username', 0)
-    session.pop('user_id', 0)
-    return redirect('/sign_in')
+@app.route('/get_id', methods=["POST"])
+def get_id():
+    result = request.get_json()
+    user_id = list(user_model.get_id(result['username'], result['password']))
+    return jsonify({'user_id': user_id[0]})
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run()
